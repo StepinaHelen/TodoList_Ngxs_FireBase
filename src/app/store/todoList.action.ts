2850be527c5472fe807706fba35275f1d353b52f
StepinaHelen <stopinahelen@gmail.com>
2022-04-08 13:04:02 +0300

    add prettier

--- src/app/store/todoList.action.ts
+++ src/app/store/todoList.action.ts
@@ -1,21 +1,20 @@
 export namespace Todo {
-  
   export class FetchAll {
-    static readonly type = '[Todo] Fetch All';
+    static readonly type = '[Todo] Fetch All'
   }
-  
+
   export class Add {
-    static readonly type = '[Todo] Add';
+    static readonly type = '[Todo] Add'
     constructor(public text: any) {}
   }
 
   export class Delete {
-    static readonly type = '[Todo] Delete';
+    static readonly type = '[Todo] Delete'
     constructor(public id: string) {}
   }
 
   export class Edit {
-  static readonly type = '[Todo] Edit';
-  constructor(public id: string, public text: any) {}
-}
+    static readonly type = '[Todo] Edit'
+    constructor(public id: string, public text: any) {}
+  }
 }
