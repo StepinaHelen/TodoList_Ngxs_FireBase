d2d54065c10b8bf0ea51a82cb07f9aa0df5150ac
StepinaHelen <stopinahelen@gmail.com>
2022-04-08 14:15:10 +0300

    fix added item list

--- src/app/service/helper.service.ts
+++ src/app/service/helper.service.ts
@@ -1,27 +1,32 @@
-import { Injectable } from '@angular/core'
-import { StateContext } from '@ngxs/store'
-import { TodoStateModel } from '../store/todolist.state'
+import { Injectable } from "@angular/core";
+import { StateContext } from "@ngxs/store";
+import { TodoStateModel } from "../store/todolist.state";
 
 @Injectable({
-  providedIn: 'root',
+    providedIn: "root",
 })
 export class HelperService {
-  constructor() {}
+    constructor() {}
 
-  manageStateChanges(list: any[], ctx: StateContext<any>) {
-    const state = ctx.getState().list
-    let newlistState: any = []
-    list.map((el) => {
-      if (el.type === 'added') {
-        newlistState.push(el)
-      } else if (el.type === 'removed') {
-        newlistState = state.filter((data: any) => data.id != el.id)
-      } else if (el.type === 'modified') {
-        newlistState = state.map((data: any) => {
-          return data.id === el.id ? el : data
-        })
-      }
-    })
-    ctx.patchState({ list: newlistState })
-  }
+    manageStateChanges(list: any[], ctx: StateContext<any>) {
+        const state = ctx.getState().list;
+        let newlistState: any = [];
+        list.map((el) => {
+            if (el.type === "added") {
+                if (!state.length) {
+                    newlistState.push(el);
+                } else {
+                    state.push(el);
+                    newlistState = state;
+                }
+            } else if (el.type === "removed") {
+                newlistState = state.filter((data: any) => data.id != el.id);
+            } else if (el.type === "modified") {
+                newlistState = state.map((data: any) => {
+                    return data.id === el.id ? el : data;
+                });
+            }
+        });
+        ctx.patchState({ list: newlistState });
+    }
 }
